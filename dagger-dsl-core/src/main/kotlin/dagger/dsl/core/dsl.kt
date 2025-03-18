11756804e892c7003ecbcc78686bb7bcbf9cb291
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-03-18 21:59:41 +0300

    Add Component and Module classes with singleton and binds functions.

--- dagger-dsl-core/src/main/kotlin/dagger/dsl/core/dsl.kt
+++ dagger-dsl-core/src/main/kotlin/dagger/dsl/core/dsl.kt
@@ -1,4 +1,26 @@
 package dagger.dsl.core
 
-fun component() {
+fun component(dsl: Component.() -> Unit): Component {
+    return Component().apply(dsl)
+}
+
+class Component {
+
+    private var isSingleton = false
+    private var name: String? = null
+    private var modules: List<Module> = mutableListOf()
+    fun singleton() {
+        isSingleton = true
+    }
+
+}
+
+
+class Module(val name: String) {
+
+    val binds: MutableList<Pair<Class<*>, Class<*>>> = mutableListOf()
+
+    inline fun <reified T> binds(classImpl: Class<out T>) {
+        binds.add(T::class.java to classImpl)
+    }
 }
